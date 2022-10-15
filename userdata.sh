28e8bb804cf64b56166c73a3c7d603c3798bdea7
Barkın <109108512+barkinkzlkaya@users.noreply.github.com>
2022-10-15 18:48:53 +0300

    settings updated

--- userdata.sh
+++ userdata.sh
@@ -4,12 +4,12 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN=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
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+git clone https://$TOKEN@https://github.com/barkinkzlkaya/blogproject.git
+cd /home/ubuntu/blogproject
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/blogproject/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
